21cc214f18ee45dffeee5eb9caadbbac1ae72bc1
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-12 11:37:00 +0600

    delete order by id added

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
             res.json(result);
         });
 
+        // DELETE order by id
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        });
+
         // ---------------------------Users API---------------------------
         // Create new user
         app.post('/users', async (req, res) => {
